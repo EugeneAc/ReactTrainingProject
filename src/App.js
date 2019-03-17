a24908cea6e00a62ad467936b84385acb969daa6
yevgeniy.asseyev <yevgeniy.asseyev@wolterskluwer.com>
2019-03-17 18:15:34 +0600

    moved from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -9,6 +9,44 @@ const App = () => {
   );
 };
 
+function App1() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox2</h1>
+      </div>
+    );
+  }
+}
+
+const element = React.createElement("h1", {}, "Hello CodeSandbox3");
+
+class Message extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox4</h1>
+      </div>
+    );
+  }
+}
+
 export default App;
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+ReactDOM.render(<App1 />, document.getElementById("element1"));
+
+ReactDOM.render(<Hello />, document.getElementById("element2"));
+
+ReactDOM.render(element, document.getElementById("element3"));
+
+ReactDOM.render(<Message />, document.getElementById("element4"));
